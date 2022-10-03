12b7af08be3bf7ae9c6abf93700e762138032cd7
Samuel <samuel.novoa97@gmail.com>
2022-10-04 01:36:43 +0200

    Si es que ya estoy demasiado cansado...

--- UIComp/UIComp.cpp
+++ UIComp/UIComp.cpp
@@ -9,7 +9,7 @@ string getUsage() {
     return "Uso: UIComp.exe RUTA_PYUIC RUTA_A_COMPILAR[-l RUTA_QTDESIGNER RUTA_ARCHIVO [intervalo]]\n\
         RUTA_PYUIC: Ruta al ejecutable pyuic6.exe\n\
         RUTA_A_COMPILAR : Ruta del fichero o directorio que se desea compilar\n\
-        -l --link: Modo \"link\"\n\
+        -l: Modo \"link\"\n\
         RUTA_QTDESIGNER: Ruta del designer.exe\n\
         RUTA_ARCHIVO: Ruta del archivo .py o .uy actualmente seleccionado\n\
         intervalo: Intervalo en milisegundos trascurrido entre cada comprobación de los ficheros\n";
